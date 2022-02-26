3e2bda8b0a9734a4a84eef3a595fca63a14cbcc2
Lam <tunglam977@gmail.com>
2022-02-26 16:42:05 +0700

    (feat) start project

--- e_commerce/lib/components/form_error.dart
+++ e_commerce/lib/components/form_error.dart
@@ -3,7 +3,9 @@ import "package:flutter/material.dart";
 
 // Form lỗi
 
-import 'package:flutter_svg/flutter_svg.dart';class FormError extends StatelessWidget {
+import 'package:flutter_svg/flutter_svg.dart';
+
+class FormError extends StatelessWidget {
   const FormError({
     Key? key,
     required this.errors,
@@ -15,27 +17,28 @@ import 'package:flutter_svg/flutter_svg.dart';class FormError extends StatelessW
   Widget build(BuildContext context) {
     return Column(
       // Sinh ra 1 danh sách cách lỗi
-      children: List.generate(errors.length, (index) => formErrorText(error: errors[index])),
+      children: List.generate(
+          errors.length, (index) => formErrorText(error: errors[index])),
     );
   }
 
   // Giao diện 1 lỗi
   Row formErrorText({String error = ""}) {
     return Row(
-        children: <Widget>[
-          // 1
-          SvgPicture.asset(
-            "assets/icons/Error.svg",
-            height: SizeConfig.getProportionateScreenHeight(16),
-            width: SizeConfig.getProportionateScreenWidth(16),
-          ),
+      children: <Widget>[
+        // 1
+        SvgPicture.asset(
+          "assets/icons/Error.svg",
+          height: SizeConfig.getProportionateScreenHeight(16),
+          width: SizeConfig.getProportionateScreenWidth(16),
+        ),
 
-          // 2
-          SizedBox(width: SizeConfig.getProportionateScreenWidth(10)),
+        // 2
+        SizedBox(width: SizeConfig.getProportionateScreenWidth(10)),
 
-          // 3
-          Text(error)
-        ],
-      );
+        // 3
+        Text(error)
+      ],
+    );
   }
 }
---
+++ video_player/lib/configs/helpers/video_info.dart
@@ -0,0 +1,224 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
+import "colors.dart" as color;
+
+class VideoInfo extends StatefulWidget {
+  const VideoInfo({Key? key}) : super(key: key);
+
+  @override
+  _VideoInfoState createState() => _VideoInfoState();
+}
+
+class _VideoInfoState extends State<VideoInfo> {
+  List videoInfo = [];
+
+  _initData() async {
+    await DefaultAssetBundle.of(context)
+        .loadString("json/videoinfo.json")
+        .then((value) {
+      setState(() {
+        videoInfo = json.decode(value);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+    //_onTapVideo(-1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          color.AppColor.gradientFirst.withOpacity(0.9),
+          color.AppColor.gradientSecond
+        ],
+        begin: const FractionalOffset(0.0, 0.4),
+        end: Alignment.topRight,
+      )),
+      child: Column(
+        children: [
+          Container(
+              padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Get.back();
+                        },
+                        child: Icon(Icons.arrow_back_ios,
+                            size: 20,
+                            color: color.AppColor.secondPageIconColor),
+                      ),
+                      Expanded(child: Container()),
+                      Icon(Icons.info_outline,
+                          size: 20, color: color.AppColor.secondPageIconColor),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "Legs Toning",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: color.AppColor.secondPageTitleColor),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "and Glutes Workout",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: color.AppColor.secondPageTitleColor),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                              colors: [
+                                color.AppColor
+                                    .secondPageContainerGradient1stColor,
+                                color.AppColor
+                                    .secondPageContainerGradient2ndColor
+                              ],
+                              begin: Alignment.bottomLeft,
+                              end: Alignment.topRight,
+                            )),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.timer,
+                              size: 20,
+                              color: color.AppColor.secondPageIconColor,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "68 min",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: color.AppColor.secondPageIconColor),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 250,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                              colors: [
+                                color.AppColor
+                                    .secondPageContainerGradient1stColor,
+                                color.AppColor
+                                    .secondPageContainerGradient2ndColor
+                              ],
+                              begin: Alignment.bottomLeft,
+                              end: Alignment.topRight,
+                            )),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.handyman_outlined,
+                              size: 20,
+                              color: color.AppColor.secondPageIconColor,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "Resistent band, kettebell",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: color.AppColor.secondPageIconColor),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              )),
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topRight: Radius.circular(70))),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      "Circuit 1: Legs Toning",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: color.AppColor.circuitsColor),
+                    ),
+                    Expanded(child: Container()),
+                    Row(
+                      children: [
+                        Icon(Icons.loop,
+                            size: 30, color: color.AppColor.loopColor),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "3 sets",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: color.AppColor.setsColor,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ))
+        ],
+      ),
+    ));
+  }
+}
