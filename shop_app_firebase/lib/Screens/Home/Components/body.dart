ed481aa29b37699e3b0ed0cbde7d05cb5439daab
Hoang Lam <tunglam977@gmail.com>
2022-04-01 15:57:15 +0700

    (feat) done video 27

--- shop_app_firebase/lib/Screens/Home/Components/body.dart
+++ shop_app_firebase/lib/Screens/Home/Components/body.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
-  const Body({ Key? key }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          title: Text("Silver appbar tutorial"),
+          centerTitle: true,
+          expandedHeight: 200,
+          backgroundColor: Colors.red,
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Text("Data");
+            },
+            childCount: 4,
+          ),
+        )
+        // SliverFillRemaining(
+        //   child: Center(
+        //     child: Text(
+        //       "Hello word",
+        //     ),
+        //   ),
+        // )
+      ],
     );
   }
 }
