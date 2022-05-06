53ce61b904cfafa3cff188c8bb930897ca4b8d7b
Hoang Lam <>
2022-05-06 17:20:16 +0700

    (feat) done video 38

--- shop_app_firebase/lib/Screens/Feeds/Components/body.dart
+++ shop_app_firebase/lib/Screens/Feeds/Components/body.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:shop_app_firebase/Screens/Feeds/Components/feeds_product.dart';
 
 class Body extends StatelessWidget {
-  const Body({ Key? key }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FeedsProduct(),
+      child: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 240 / 290,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        children: List.generate(100, (index) {
+          return FeedsProduct();
+        }),
+      ),
     );
   }
 }
